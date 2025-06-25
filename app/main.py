9a58f9a51b903a38a882aad1ce96ca05a8c34555
dathoangquoc <dathgqc@gmail.com>
2025-06-25 17:22:05 +0700

    wip input guardrail

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import os
 import asyncio
 
+# Pydantic
+from pydantic import BaseModel
+
 # OpenAI Agent
 
 from agents.extensions.models.litellm_model import LitellmModel
@@ -8,8 +11,15 @@ from agents import (
     Agent, 
     Runner, 
     function_tool,
-    set_tracing_disabled
+    set_tracing_disabled,
+    GuardrailFunctionOutput,
+    input_guardrail,
+    RunContextWrapper,
+    TResponseInputItem,
+    InputGuardrailTripwireTriggered
 )
+
+# mem0
 
 from mem0 import Memory
 
@@ -34,13 +44,13 @@ def search_web(query: str):
     """Search online sources"""
     return f"Found no info"
 
-# Memory
+# TODO: mem0 config
 config = {
     "llm": {
 
     },
 }
-m = Memory()
+# m = Memory()
 
 def add_memory(user: str):
     pass
@@ -53,16 +63,68 @@ def search_memory(user: str):
 model = LitellmModel(model=f"openai/{MODEL_NAME}", base_url=BASE_URL, api_key=API_KEY)
 set_tracing_disabled(True)
 
-agent = Agent(
-    name="Main Agent",
+### Guardrail
+guardrail_agent = Agent(
+    name="Input Guardrail",
+    instructions="Check if the user is asking questions unrelated to the career service",
     model=model,
-    instructions="You are a helpful AI",
+)
+
+class CareerServiceOutput():
+    reasoning: str
+    is_career_inquiry: bool
+
+
+@input_guardrail
+async def career_guardrail(
+    agent: Agent, input: str | list[TResponseInputItem], context: RunContextWrapper[None]
+) -> GuardrailFunctionOutput:
+    """Check if the input is unrelated to the career service
+    """
+    result = await Runner.run(agent, input, context=context)
+    final_output = result.final_output_as(CareerServiceOutput)
+
+    return GuardrailFunctionOutput(
+        output_info=final_output,
+        tripwire_triggered=final_output.is_career_inquiry
+    )
+
+### Main Agent
+
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="Handoff to the appropriate agent based on the user's requested service(s)",
+    model=model,
+    handoffs=[],
+    input_guardrails=[career_guardrail],
     tools=[schedule_meeting, search_document, search_web],
 )
 
+
 async def main():
-    result = await Runner.run(agent, "Search for documents on LLM")
-    print(result)
+    input_data: list[TResponseInputItem] = []
+
+    while True:
+        user_input = input("Enter a message: ")
+        input_data.append(
+            {
+                "role": "user",
+                "content": user_input
+            }
+        )
+
+        try:
+            result = await Runner.run(triage_agent, input_data)
+            print(result.final_output)
+        except InputGuardrailTripwireTriggered:
+            message = "Sorry I can't help with unrelated inquiries"
+            print(message)
+            input_data.append(
+                {
+                    "role": "assistant",
+                    "content": message
+                }
+            )
 
 
 if __name__ == "__main__":
