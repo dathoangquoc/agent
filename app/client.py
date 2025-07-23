f6799b279501409c4d07d95da291ec36c5a3f4b5
dathoangquoc <dathgqc@gmail.com>
2025-07-23 10:21:34 +0700

    structure function

--- app/client.py
+++ app/client.py
@@ -1,14 +1,24 @@
-from litellm import completion
+from litellm import completion, batch_completion
 
 class LiteLLMClient():
     
     model: str
+    api_key: str
 
     def complete(self, messages: list[dict[str:str]] = []):
-        return completion(model=self.model, messages=messages)
+        # TODO: prepare input: sampling options, custom_llm_provider, model
+        
+        response = completion(
+            model=self.model, 
+            messages=messages,
+        )
+
+        # TODO: handle failure, reasoning, tool, logging
+        return response
 
     async def stream(self, messages: list[dict[str:str]] = []):
-        return completion(model=self.model, messages=messages)
+        return completion(model=self.model, messages=messages, stream=True)
 
-    def batch_complete():
-        pass
+    # Multiple call to 1 model
+    def batch_complete(self, messages: list[dict[str:str]] = []):
+        return batch_completion(model=self.model, messages=messages)
