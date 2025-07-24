3ed5e896cf16eddff3b78f0c1bc8312ddbd504b2
dathoangquoc <dathgqc@gmail.com>
2025-07-24 09:36:20 +0700

    prep input

--- app/client.py
+++ app/client.py
@@ -4,16 +4,38 @@ class LiteLLMClient():
     
     model: str
     api_key: str
+    base_url: str
+    custom_llm_provider: str = None
 
-    def complete(self, messages: list[dict[str:str]] = []):
-        # TODO: prepare input: sampling options, custom_llm_provider, model
+    def complete(
+            self, 
+            messages: list[dict[str:str]] = [], 
+            temperature: float = None,
+            top_p: float = None,
+            max_tokens: int = None,
+        ) -> str:
         
         response = completion(
             model=self.model, 
             messages=messages,
+            temperature=temperature,
+            top_p=top_p,
+            max_tokens=max_tokens,
+            base_url=self.base_url,
+            api_key=self.api_key,
+            custom_llm_provider=self.custom_llm_provider,
         )
 
-        # TODO: handle failure, reasoning, tool, logging
+        # Handle failure
+
+        # Parse reasoning
+
+        # Parse tool
+
+        # Logging:
+        # https://docs.litellm.ai/docs/completion/token_usage
+        # https://docs.litellm.ai/docs/observability/callbacks
+
         return response
 
     async def stream(self, messages: list[dict[str:str]] = []):
