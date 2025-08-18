0ae9949386ef4d55682285d85aff1bfce8d6dfcb
dathoangquoc <dathgqc@gmail.com>
2025-08-18 15:02:34 +0700

    note bug

--- app/agent.py
+++ app/agent.py
@@ -43,34 +43,38 @@ class ChatWithMemory:
         """
         Start a chat loop
         """
-        messages = []
-        while True:
-            user_input = input("You: ")
-            if user_input.lower() in ["exit", "quit", "q"]:
-                print("Exiting chat...")
-                break
+        try:
+            messages = []
+            while True:
+                user_input = input("You: ")
+                if user_input.lower() in ["exit", "quit", "q"]:
+                    print("Exiting chat...")
+                    break
 
-            user_message = {"role": "user", "content": user_input}
+                user_message = {"role": "user", "content": user_input}
 
-            messages.append(user_message)
-            self.history.append(user_message)
+                messages.append(user_message)
+                self.history.append(user_message)
 
-            result = Runner.run_streamed(self.starting_agent, input=user_input)
+                result = Runner.run_streamed(self.starting_agent, input=user_input)
 
-            print("\nAgent: ")
-            async for event in result.stream_events():
-                if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-                    print(event.data.delta, end="", flush=True)
-            
-            response_message = {"role": "assistant", "content": result.final_output}
+                print("\nAgent: ")
+                async for event in result.stream_events():
+                    if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+                        print(event.data.delta, end="", flush=True)
+                
+                response_message = {"role": "assistant", "content": result.final_output}
 
-            messages.append(response_message)
-            self.history.append(response_message)
-            
-            print("-------------------------------------------------------")
-        
-        
-    
+                messages.append(response_message)
+                self.history.append(response_message)
+                
+                print("-------------------------------------------------------")
+        finally:
+            # BUG: 
+            # Unclosed client session
+            # client_session: <aiohttp.client.ClientSession object at 0x7a2981fe46b0>
+            pass
+
     def mock_chat(prev_dialog: str, next_dialog: str):
         """
         Simulate 2 distinct dialog sessions of 1 user 
